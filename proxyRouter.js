218888570cf224c677260f8032c0a27c73d0e9fa
Paul Berry <paul.berry@prettylittlething.com>
2016-03-13 18:09:16 +0000

    initial commit

---
+++ proxyRouter.js
@@ -0,0 +1,60 @@
+//var redis     = require("redis");
+//var client = redis.createClient({host:'nodeploy.kvusam.0001.usw2.cache.amazonaws.com'});
+
+var ProxyRouter = function(options) {
+
+
+
+  if (!options.backend) {
+    throw "ProxyRouter backend required. Please provide options.backend parameter!";
+  }
+
+  this.backend   = options.backend;
+  this.cache_ttl = (options.cache_ttl || 10) * 1000;
+  this.cache     = {};
+
+  console.log("ProxyRouter cache TTL is set to " + this.cache_ttl + " ms.");
+};
+
+ProxyRouter.prototype.lookup = function(hostname, callback) {
+  var self = this;
+  if (!this.cache[hostname]) {
+    this.backend.hget('routes', hostname, function(err, data) {
+      if (data) {
+        // Lookup route
+        var route = data.split(':');
+        var target = {host: route[0], port: route[1]};
+
+        // Set cache and expiration
+        self.cache[hostname] = target;
+        self.expire_route(hostname, self.cache_ttl);
+
+        // Return target
+        callback(target);
+      }
+      else {
+        callback(null);
+      }
+    });
+  }
+  else {
+    callback(this.cache[hostname]);
+  }
+};
+
+ProxyRouter.prototype.flush = function() {
+  this.cache = {};
+};
+
+ProxyRouter.prototype.flush_route = function(hostname) {
+  delete(this.cache[hostname]);
+};
+
+ProxyRouter.prototype.expire_route = function(hostname, ttl) {
+  var self = this;
+  setTimeout(function() {
+    self.flush_route(hostname);
+  }, ttl);
+};
+
+exports.ProxyRouter = ProxyRouter;
---
+++ server.js
@@ -0,0 +1,61 @@
+
+var redis_host = 'nodeploy.kvusam.0001.usw2.cache.amazonaws.com'; //TODO: move to config file
+
+var redis     = require("redis"),
+    http      = require('http'),
+    httpProxy = require('http-proxy');
+    ProxyRouter = require('./proxyRouter.js');
+    proxy = httpProxy.createProxyServer({}),
+    url = require('url');
+
+
+
+var proxyRouter = new ProxyRouter.ProxyRouter({
+  backend: redis.createClient({host:redis_host}),
+  cache_ttl: 5
+});
+
+
+//Test routes and servers
+console.log('adding test routes');
+var redisClient = redis.createClient({host:redis_host});
+console.log('adding test1.nodeploy.it');
+redisClient.hset('routes','test1.nodeploy.it','52.37.45.73:80');
+redisClient.hset('routes','balls.nodeploy.it','52.37.45.73:80');
+http.createServer(function(req, res) {
+    res.end("Hello world from test1 running on 127.0.0.1:9001");
+}).listen(9001);
+
+console.log('adding test2.nodeploy.it');
+redisClient.hset('routes','test2.nodeploy.it','127.0.0.1:9002');
+http.createServer(function(req, res) {
+    res.end("Hello world from test2 running on 127.0.0.1:9002");
+}).listen(9002);
+
+
+//proxy server
+http.createServer(function(req, res) {
+    var hostname = req.headers.host.split(":")[0];
+    var pathname = url.parse(req.url).pathname;
+
+    console.log(hostname);
+    console.log(pathname);
+
+    proxyRouter.lookup(hostname, function(route) {
+        if (route) {
+          console.log('routing '+hostname+' to ' + 'http://'+route.host+':'+route.port);
+          proxy.web(req, res, { target: 'http://'+route.host+':'+route.port });
+        } else {
+          console.log('no route');
+          try {
+            res.writeHead(404);
+            res.end("No route found");
+          }
+          catch (er) {
+            console.error("res.writeHead/res.end error: %s", er.message);
+          }
+        }
+    });
+}).listen(8000, function() {
+    console.log('proxy listening on port 8000');
+});
